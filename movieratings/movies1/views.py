f4b146e19e9d620dca7f1dd86edce7b7fd9ff18c
kaslemr <mattkasle@gmail.com>
2015-10-26 15:47:49 -0400

    Changed to functions

--- movieratings/movies1/views.py
+++ movieratings/movies1/views.py
@@ -10,12 +10,12 @@ def index_view(request):
     context = {}
     return render_to_response(template_name="base.html", context=context)
 
-def movie_list_view(request):
-    all_movies = Movie.objects.all()
-    return render_to_response(template_name="movie_list.html", context={"movie_list": all_movies})
+class MovieList(ListView):
+    model = Movie
+    template_name = "movie_list.html"
 
-def movie_detail_view(request, movie_id):
-    movies = Movie.objects.get(id=movie_id)
-    ratings = movies.rating_set.all()
-    context={"movie_object": movies, "ratings": ratings}
-    return render_to_response(template_name="movie_detail.html", context=context)
+
+class MovieDetail(DetailView):
+    model = Movie
+    template_name = "movie_detail.html"
+
